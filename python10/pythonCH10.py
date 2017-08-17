48dc1340f361c76ada52cdfdaae835b042a93cd8
orbicularis <misc@komputermatrix.com>
2017-08-17 14:37:45 -0700

    current demos

--- python10/pythonCH10.py
+++ python10/pythonCH10.py
@@ -1,132 +1,52 @@
-##fr2eng = dict()
-##fr2eng['one'] = 'un'
-##print(fr2eng['one'])
-##vals = list(fr2eng.values())
+##EXAMPLES
+##t1 = ('a',)
+##print(type(t1))
 
-##word = 'brontosaurus'
-##d = dict()
-##for letter in word:
-##    if letter not in d:
-##        d[letter] = 1
-##    else:
-##        d[letter] = d[letter] + 1
-##
-##print(d)
+##t = tuple('lupins')
+##print(t[3:5])
 
-##counts = { 'chuck': 1, 'annie': 42, 'jan': 100}
-##print(counts.get('annie', 0))
+##txt = 'but soft what light in yonder window breaks'
+##words = txt.split()
+####print(words)
+##t = list()
+##for word in words:
+##    t.append((len(word), word))
+####print(t)
+##t.sort(reverse=True)
+##res = list()
+##for length, word in t:
+##    res.append(word)
+##print(res)
 
-##word = 'brontosaurus'
-##d = dict()
-##for letter in word:
-##    d[letter] = d.get(letter, 0) + 1
-##print(d)
+##addr = 'monty@python.org'
+##uname, domain = addr.split('@')
+##print('user:', uname, '\ndomain:', domain)
 
+##d = {'a':10, 'b':1, 'c':22}
+##l = list()
+##for key, val in d.items():
+##    l.append( (val, key) )
+##l.sort(reverse=True)
+##print(l)
 
-## EXERCISE 2 - this searches the text file for a from and pulls a word out
-
-##mbox = open('mbox-short.txt')
-##count = dict()
-##
-##for line in mbox:
-##    newLine = line.find('From ')
-##    if newLine is not 0:
-##        continue
-##    else:
-####        print(line)
-##        line = line.split()
-##        newLine = line[2]
-####        print(newLine)
-##        
-##        for days in newLine.split():
-##            if days not in count:
-##                count[days] = 1
-##            else:
-##                count[days] += 1
-##
-##print(count)
-
-##EXERCISE 3 - thsi is a histogram of email addresses
-##
-##mbox = open('mbox-short.txt')
-##count = dict()
-##
-##for line in mbox:
-##    newLine = line.find('From ')
-##    if newLine is not 0:
-##        continue
-##    else:
-####        print(line)
-##        line = line.split()
-##        newLine = line[1]
-####        print(newLine)
-##        
-##        for email in newLine.split():
-##            if email not in count:
-##                count[email] = 1
-##            else:
-##                count[email] += 1
-##
-##print(count)
-
-##EXERCISE 4 - same as 3 except finds highest sender
-##
-##mbox = open('mbox-short.txt')
-##count = dict()
-##
-##for line in mbox:
-##    newLine = line.find('From ')
-##    if newLine is not 0:
-##        continue
-##    else:
-####        print(line)
-##        line = line.split()
-##        newLine = line[1]
-####        print(newLine)
-##        
-##        for email in newLine.split():
-##            if email not in count:
-##                count[email] = 1
-##            else:
-##                count[email] += 1
-##
-##
-##hiScore = 0
-##key = None
-##for score in count:
-##    if count[score] > hiScore:
-##        hiScore = count[score]
-##        entry = score
-##print(entry, hiScore)
-
-##EXERCISE 5 - same as 3 except finds highest sender account
-
-mbox = open('mbox-short.txt')
-count = dict()
-
-for line in mbox:
-    newLine = line.find('From ')
-    if newLine is not 0:
-        continue
-    else:
-##        print(line)
-        amper = line.find('@')
-##        print(amper)
-        endspot = line.find(' ',amper)
-##        print(endspot)
-        domain = line[amper+1:endspot]
-##        print(domain)
-
-        if domain not in count:
-            count[domain] = 1
+import string
+fhand = open('romeo-full.txt')
+counts = dict()
+for line in fhand:
+    line = line.translate(str.maketrans('', '', string.punctuation))
+    line = line.lower()
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
         else:
-            count[domain] += 1
-print(count)
+            counts[word] += 1
+lst  = list()
+for key, val in list(counts.items()):
+    lst.append((val, key))
 
-##hiScore = 0
-##key = None
-##for score in count:
-##    if count[score] > hiScore:
-##        hiScore = count[score]
-##        entry = score
-##print(entry, hiScore)
+lst.sort(reverse=True)
+
+for key, val in lst[:10]:
+    print(key, val)
+    
