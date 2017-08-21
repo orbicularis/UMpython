7acdc31b3f7964a2dc03613159c13d894a235b7f
Andrew Payne <D:\Account-Andrew\Sync\Documents Win Sync\TheBat>
2017-08-21 16:39:52 -0700

    current ch10 exercises

--- python10/pythonCH10.py
+++ python10/pythonCH10.py
@@ -29,24 +29,95 @@
 ##l.sort(reverse=True)
 ##print(l)
 
-import string
-fhand = open('romeo-full.txt')
-counts = dict()
-for line in fhand:
-    line = line.translate(str.maketrans('', '', string.punctuation))
-    line = line.lower()
-    words = line.split()
-    for word in words:
-        if word not in counts:
-            counts[word] = 1
-        else:
-            counts[word] += 1
-lst  = list()
-for key, val in list(counts.items()):
-    lst.append((val, key))
-
-lst.sort(reverse=True)
-
-for key, val in lst[:10]:
-    print(key, val)
+##import string
+##fhand = open('romeo-full.txt')
+##counts = dict()
+##for line in fhand:
+##    line = line.translate(str.maketrans('', '', string.punctuation))
+##    line = line.lower()
+##    words = line.split()
+##    for word in words:
+##        if word not in counts:
+##            counts[word] = 1
+##        else:
+##            counts[word] += 1
+##lst  = list()
+##for key, val in list(counts.items()):
+##    lst.append((val, key))
+##
+##lst.sort(reverse=True)
+##
+##for key, val in lst[:10]:
+##    print(key, val)
     
+####EXERCISE 1 - ADDRESSES PULLED FROM FILE
+####import string
+##mbox = open('mbox-short.txt')
+##counts = dict()
+##winner = list()
+##
+##for line in mbox:
+##    newLine = line.find('From ')
+##    if newLine is not 0:
+##        continue
+##    else:
+####        print(line)
+##        amper = line.find('@')
+##        endSpace = line.find(' ', amper)
+##        domains = line[amper+1 : endSpace]
+####        print(domain)
+##
+##        if domains not in counts:
+##            counts[domains] = 1
+##        else:
+##            counts[domains] += 1
+##
+##for x, y in counts.items():
+##    winner.append((y, x))
+##    
+##print(max(winner))
+
+##EXERCISE 1 - ADDRESSES PULLED FROM FILE
+##import string
+mbox = open('mbox-short.txt')
+counts = dict()
+winner = list()
+
+for line in mbox:
+    newLine = line.find('From ')
+    if newLine is not 0:
+        continue
+    else:
+##        print(line)
+        amper = line.find('@')
+        endSpace = line.find(' ', amper)
+        domains = line[amper+1 : endSpace]
+##        print(domain)
+
+        if domains not in counts:
+            counts[domains] = 1
+        else:
+            counts[domains] += 1
+
+for x, y in counts.items():
+    winner.append((y, x))
+    
+print(max(winner))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
