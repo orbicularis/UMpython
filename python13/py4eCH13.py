54e6190fb005fca08485ed6eed4794e14b613aff
orbicularis <misc@komputermatrix.com>
2017-09-22 15:37:34 -0700

    current ch13

--- python13/py4eCH13.py
+++ python13/py4eCH13.py
@@ -6,13 +6,15 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = ('http://py4e-data.dr-chuck.net/known_by_Christian.html')
+url = ('http://py4e-data.dr-chuck.net')
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 bowl = []
 bigCount = 0
 count = 0
 tags = soup('a')
+print(tags)
+
 
 for tag in tags:
    if count < 18:
@@ -21,31 +23,31 @@ for tag in tags:
       bowl.append(tag)
       count += 1
 
-spoon = str(bowl)
-cup = spoon.split('"')
-newLink = cup[1]
-print(newLink)
-bigCount += 1
-
-while bigCount < 7:
-   url = newLink
-   html = urllib.request.urlopen(url, context=ctx).read()
-   soup = BeautifulSoup(html, 'html.parser')
-   bowl = []
-   count = 0
-   tags = soup('a')
-
-   for tag in tags:
-      if count < 18:
-##         print(tag2.get('href', None))
-         bowl.clear()
-         bowl.append(tag)
-         count += 1
-
-   spoon = str(bowl)
-   cup = spoon.split('"')
-   newLink = cup[1]
-   print(newLink)
-   bigCount += 1
-
-print('done')
+##spoon = str(bowl)
+##cup = spoon.split('"')
+##newLink = cup[1]
+##print(newLink)
+##bigCount += 1
+##
+##while bigCount < 7:
+##   url = newLink
+##   html = urllib.request.urlopen(url, context=ctx).read()
+##   soup = BeautifulSoup(html, 'html.parser')
+##   bowl = []
+##   count = 0
+##   tags = soup('a')
+##
+##   for tag in tags:
+##      if count < 18:
+####         print(tag2.get('href', None))
+##         bowl.clear()
+##         bowl.append(tag)
+##         count += 1
+##
+##   spoon = str(bowl)
+##   cup = spoon.split('"')
+##   newLink = cup[1]
+##   print(newLink)
+##   bigCount += 1
+##
+##print('done')
