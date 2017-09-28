105c24f4de424407a55aaa732063996c6eb0befa
orbicularis <misc@komputermatrix.com>
2017-09-28 10:12:05 -0700

    ch14 in progress

---
+++ python14/party1.py
@@ -0,0 +1,52 @@
+##stuff = list()
+##stuff.append('python')
+##stuff.append('chuck')
+##stuff.sort()
+##print (stuff[0])
+##
+##print(stuff.__getitem__(0))
+##print(list.__getitem__(stuff,0))
+
+##stuff = list()
+##print(dir(stuff))
+
+##class PartyAnimal:
+##    x = 0
+##
+##    def party(self) :
+##        self.x = self.x + 1
+##        print('So far:',self.x)
+##        
+##an = PartyAnimal()
+##an.party()
+##an.party()
+##an.party()
+##PartyAnimal.party(an)
+
+#### just having a little fun with type
+##franz = list
+##webster = dict
+##
+##print(type(franz))
+##print(type(webster))
+
+## THIS IS party5.py
+class PartyAnimal:
+   x = 0
+   name = ''
+   def __init__(self, nam):
+     self.name = nam
+     print(self.name,'was constructed')
+
+   def party(self) :
+     self.x = self.x + 1
+     print(self.name,'party count',self.x)
+
+s = PartyAnimal('Sally')
+s.party()
+j = PartyAnimal('Jim')
+j.party()
+s.party()
+
+
+
