b8d1c6f94a845835f2f26b19f47b46009290a468
orbicularis <misc@komputermatrix.com>
2017-09-28 17:25:00 -0700

    initial commit for ch15

---
+++ python15/ch15ex.py
@@ -0,0 +1,14 @@
+## THIS IS db1.py A BASIC DATABASE PROGRAM
+
+import sqlite3
+
+conn = sqlite3.connect('music.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Tracks')
+cur.execute('CREATE TABLE Tracks (title TEXT, plays INTEGER)')
+
+conn.close()
+
+
+
